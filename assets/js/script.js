d2ef063a57ac2dab0d71f3037141cdc1db9575c2
Marcos Abreu <marcos.abreu.oliveira@gmail.com>
2020-12-13 19:44:16 -0300

    update project

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,13 @@
     SlidesToScroll: 5,
     draggable: true,
   })
+
+  window.addEventListener('load', () => {
+    const $modal = document.querySelector('.modal')
+    const $modalClose = document.querySelector('.modal-close-button')
+
+    $modal.style.display = 'block';
+    $modalClose.addEventListener('click', () => {
+      $modal.style.display = 'none';
+    })
+  });
