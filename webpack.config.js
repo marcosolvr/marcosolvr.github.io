f01d2a34eb4bb42f253787629b51a3e2e129e59d
Marcos Abreu <marcos.abreu.oliveira@gmail.com>
2020-12-15 10:07:53 -0300

    chore: webpack config

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.scss$/,
+        use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true,
+                // options...
+              }
+            }
+          ]
+      },
+      {
+        exclude: /node_modules/,
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env']
+          }
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/style.css'
+    }),
+  ]
+};
