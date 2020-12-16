fed55de3fcb37318996d416394405ae7bc9e7811
Marcos Abreu <marcos.abreu.oliveira@gmail.com>
2020-12-16 11:17:34 -0300

    feat: add most requested section

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -7,11 +7,12 @@
   \*********************************/
 /***/ (() => {
 
+var bannerDraggable = screen.width < 1150 ? true : false;
 new Glider(document.querySelector('.js-banner-carousel'), {
   slideToShow: 1,
   SlidesToScroll: 1,
   dots: '.dots',
-  draggable: true,
+  draggable: bannerDraggable,
   arrows: {
     prev: '.js-banner-carousel-prev',
     next: '.js-banner-carousel-next'
@@ -30,6 +31,16 @@ if (screen.width < 1550) {
     draggable: true
   });
 }
+
+new Glider(document.querySelector('.js-most-requested'), {
+  slidesToShow: 6,
+  slidesToScroll: 2,
+  draggable: bannerDraggable,
+  arrows: {
+    prev: '.js-most-requested-prev',
+    next: '.js-most-requested-next'
+  }
+});
 
 /***/ }),
 
--- src/_javascript/main.js
+++ src/_javascript/main.js
@@ -1,8 +1,10 @@
+const bannerDraggable = screen.width < 1150 ? true : false
+
 new Glider(document.querySelector('.js-banner-carousel'), {
   slideToShow: 1,
   SlidesToScroll: 1,
   dots: '.dots',
-  draggable: true,
+  draggable: bannerDraggable,
   arrows: {
     prev: '.js-banner-carousel-prev',
     next: '.js-banner-carousel-next'
@@ -22,3 +24,13 @@ if (screen.width < 1550) {
     draggable: true,
   })
 }
+
+new Glider(document.querySelector('.js-most-requested'), {
+  slidesToShow: 6,
+  slidesToScroll: 2,
+  draggable: bannerDraggable,
+  arrows: {
+    prev: '.js-most-requested-prev',
+    next: '.js-most-requested-next'
+  }
+})
