11b983f2347d306427276f7fe009b4b47bf4645f
Marcos Abreu <marcos.abreu.oliveira@gmail.com>
2020-12-16 09:04:02 -0300

    feat: add reasons to buy section

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -7,15 +7,21 @@
   \*********************************/
 /***/ (() => {
 
+var draggable = screen.width < 1550 ? true : false;
 new Glider(document.querySelector('.js-banner-carousel'), {
   slideToShow: 1,
   SlidesToScroll: 1,
   dots: '.dots',
-  draggable: true,
+  draggable: draggable,
   arrows: {
     prev: '.js-banner-carousel-prev',
     next: '.js-banner-carousel-next'
   }
+});
+new Glider(document.querySelector('.js-reasons-to-buy-boxes'), {
+  slidesToShow: 5,
+  slidesToScroll: 5,
+  draggable: draggable
 });
 
 /***/ }),
